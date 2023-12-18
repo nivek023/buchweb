68c0f912a495c58106da18e14777657976244c46
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-12-19 00:43:03 +0100

    Login with the Navbar is now working completely

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,36 +1,76 @@
-import { AppBar, Toolbar, Button, Typography, TextField } from "@mui/material";
-import { styled } from "@mui/system";
-import { useNavigate } from "react-router-dom";
-import SearchIcon from '@mui/icons-material/Search';
-import { useState } from 'react';
-import { useAuth } from "../provider/useAuth";
-
-
+import { AppBar, Toolbar, Button, Typography, TextField } from '@mui/material'
+import { styled } from '@mui/system'
+import { useNavigate } from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search'
+import { useState, useEffect } from 'react'
+import { useAuth } from '../provider/useAuth'
 
 const Navbar = () => {
-  const Grow = styled("div")({
+  const Grow = styled('div')({
     flexGrow: 1,
-  });
-  const [benutzer, setBenutzer] = useState("");
-  const [passwort, setPasswort] = useState("");
+  })
+  const [benutzer, setBenutzer] = useState('');
+  const [passwort, setPasswort] = useState('');
   const navigate = useNavigate();
-  const {login, rolle} = useAuth();
+  const { login, logout, cToken } = useAuth();
+  const [navbarColor, setNavbarColor] = useState('default');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const handleLogoClick = () => {
-    navigate("/");
-  };
+    navigate('/');
+  }
 
   const handleSearchClick = () => {
-    navigate("/search");
-  };
-  const handleLoginClick =  () => {
-    login(benutzer, passwort);
-  };
+    navigate('/search');
+  }
+  const handleLoginClick = async () => {
+    if (isLoggedIn) {
+      logout();
+      setIsLoggedIn(false);
+      console.log('Navbar.handleLoginClick: logged out');
+      return;
+    }
+    try {
+    const successfulLogin = await login(benutzer, passwort);
+    setIsLoggedIn(successfulLogin);
+
+    console.log(
+      'Navbar.handleLoginClick: login executed, successfulLogin: ',
+      successfulLogin
+    );
+
+    if (successfulLogin) {
+      console.log('Login success');
+      setNavbarColor('success');
+    } else {
+      console.log('Login failed');
+      setNavbarColor('error');
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+  }
+};
+
+  useEffect(() => {
+    if (navbarColor === 'error') {
+      const timeout = setTimeout(() => {
+        setNavbarColor('default')
+      }, 220)
+      return () => clearTimeout(timeout)
+    }
+    if (navbarColor === 'success') {
+      const timeout = setTimeout(() => {
+        setNavbarColor('default')
+      }, 220)
+      return () => clearTimeout(timeout)
+    }
+  }, [navbarColor])
 
   return (
-    <AppBar position="fixed">
+    <AppBar position="fixed" color={navbarColor}>
       <Toolbar>
         <Button onClick={handleLogoClick} color="inherit">
-          AutoLogo
+          BuchLogo
         </Button>
         <Button onClick={handleSearchClick} variant="contained" color="primary">
           Erweiterte Suche <SearchIcon />
@@ -44,32 +84,36 @@ const Navbar = () => {
           />
         </Typography>
         <Grow />
-        <Typography variant="h6" component="div">
-          <TextField
-            id="navbar-benutzer"
-            label="Benutzer"
-            variant="outlined"
-            color="secondary"
-            value={benutzer}
-            onChange={(e) => setBenutzer(e.target.value)}
-          />
-        </Typography>
-        <Typography variant="h6" component="div">
-          <TextField
-            id="navbar-passwort"
-            label="Passwort"
-            variant="outlined"
-            color="secondary"
-            type="password"
-            value={passwort}
-            onChange={(e) => setPasswort(e.target.value)}
-          />
-        </Typography>
+        {isLoggedIn ? null : (
+          <>
+            <Typography variant="h6" component="div">
+              <TextField
+                id="navbar-benutzer"
+                label="Benutzer"
+                variant="outlined"
+                color="secondary"
+                value={benutzer}
+                onChange={(e) => setBenutzer(e.target.value)}
+              />
+            </Typography>
+            <Typography variant="h6" component="div">
+              <TextField
+                id="navbar-passwort"
+                label="Passwort"
+                variant="outlined"
+                color="secondary"
+                type="password"
+                value={passwort}
+                onChange={(e) => setPasswort(e.target.value)}
+              />
+            </Typography>
+          </>
+        )}
         <Button onClick={handleLoginClick} color="inherit">
-          {rolle}
+          {isLoggedIn ? 'Logout' : 'Login'}
         </Button>
       </Toolbar>
     </AppBar>
-  );
+  )
 };
 export default Navbar;
--- src/components/provider/AuthProvider.jsx
+++ src/components/provider/AuthProvider.jsx
@@ -5,50 +5,59 @@ import axios from 'axios'
 export const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState('')
+  const [cToken, setCToken] = useState('')
   const [rolle, setRolle] = useState('lesen')
 
   const login = async (username, password) => {
     try {
       const response = await loginUser({ username, password })
-      const { token, roles } = await response.json()
-      setToken(token)
-      roles.forEach((r) => {
-        if (r === 'admin') {
-          setRolle('bearbeiten')
-        }
-      })
+      console.log('AuthProvider.login: response:', response);
+      const { token, roles } = response.data
+      setCToken(token)
+      console.log('AuthProvider.login: setToken executed');
+      console.log('AuthProvider.login: token: ', token);
+      const isAdminOrFachabteilung = roles.some(
+        (r) => r === 'admin' || r === 'fachabteilung'
+      )
+      const isKunde = roles.includes('kunde')
+
+      if (isAdminOrFachabteilung) {
+        setRolle('bearbeiten')
+        return true
+      }
+      if (isKunde) {
+        return true
+      }
+      return false
     } catch (error) {
-      // TODO: Error Handling
+      console.log('AuthProvider.login: error, login nicht erfolgreich')
+      return false
     }
   }
 
   const loginUser = async ({ username, password }) => {
     const url = '/api/auth/login'
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Access-Control-Allow-Origin': '*',
-        'connection':'keep-alive',
-        'accept-encoding':'gzip,deflate, br',
-        Accept: 'text/plain',
+    const requestData = `username=${encodeURIComponent(
+      username
+    )}&password=${encodeURIComponent(password)}`
+    const response = await axios.post(url, requestData)
+    console.log('AuthProvider.loginUser: axios.post executed')
+    if (response.status === 200) {
+      console.log('AuthProvider.loginUser: (200)')
+      return response
+    } else {
+      console.log('AuthProvider.loginUser: axios.post failed')
+      throw new Error('Login failed')
     }
-    const requestData = `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
-    //TODO wenn !ok
-    const response = await axios.post(
-      url,
-      requestData,{ headers });
-    console.log(response);
-
-    return response
   }
 
   const logout = () => {
-    setToken('')
+    setCToken('')
     setRolle('lesen')
   }
 
   return (
-    <AuthContext.Provider value={{ token, rolle, login, logout }}>
+    <AuthContext.Provider value={{ cToken, rolle, login, logout }}>
       {children}
     </AuthContext.Provider>
   )
