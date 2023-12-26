9d313dd048aeb64d945e90caaf21cd2d59f29965
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-12-26 11:08:37 +0100

    Rest request for details
    Implemented the rest request for details of a book.

--- src/components/main/BookDetails.jsx
+++ src/components/main/BookDetails.jsx
@@ -1,25 +1,40 @@
 import BookDetailsForm from '../form/BookDetailsForm';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const BookDetails = () => {
-  const book = {
-    isbn: '978-3-897-22583-1',
-    rating: 0,
-    art: 'DRUCKAUSGABE',
-    preis: 11.1,
-    rabatt: 0.011,
-    lieferbar: true,
-    datum: '2022-02-01',
-    homepage: 'https://acme.at',
-    schlagwoerter: ['JAVASCRIPT'],
-    titel: {
-      titel: 'Alpha',
-      untertitel: 'alpha',
-    },
-  };
+  const [book, setBook] = useState(null);
+  const { id = 'default' } = useParams();
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      const url = '/api/rest';
+      const request = `?isbn=${id}`;
+      try {
+        const response = await axios.get(url + request);
+        if (response.status !== 200) {
+          throw new Error('BookDetails.fetchBook: Kein 200 Status-Code');
+        }
+        if (response.data) {
+          setBook(response.data._embedded.buecher[0]);
+        } else {
+          console.error('BookDetails.fetchBook: resopnse.data ist undefiniert');
+        }
+      } catch (error) {
+        console.error('BookDetails.fetchBook:', error.message);
+      }
+    };
+    fetchBook();
+  }, [id]);
+
+  if (!book) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1>Details</h1>
+      <h2>Details</h2>
       <BookDetailsForm book={book} />
     </div>
   );
