7e021baf30a56ad1c258fb65f719a05531d08c6f
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-12-28 16:28:08 +0100

    resctriced access to url-paths
    Switching to a restricted url-path with improper authorization no longer renders the related component

---
+++ src/components/main/Forbidden.jsx
@@ -0,0 +1,7 @@
+const Forbidden = () => {
+    return <div><p>You Currently have no Access to this Page.<br></br>
+                    Please Login to continue.
+        </p></div>;
+  };
+  
+  export default Forbidden;
