d4cf6d66c5dfe7e86a9328fa1725a18fb5b332c9
Kevin Rosenauer <roke1011@h-ka.de>
2023-12-30 11:04:50 +0100

    addNewBook solve but toDo CodeCleanup

--- src/components/form/AddNewBookForm.jsx
+++ src/components/form/AddNewBookForm.jsx
@@ -13,237 +13,322 @@ import {
 import BookIcon from '@mui/icons-material/Book';
 import { styled } from '@mui/system';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
+import axios from 'axios';
+import { useContext } from 'react';
 
-
-const AddNewBookForm = ({
-  addNewBook,
-  handleInputChange,
-  handleAddNewBook,
-  success,
-  error,
-}) => {
-
-  const Table = styled('table')({
-    width: '75%',
-    display: 'relative',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '0',
-    flexWrap: 'wrap',
+// eslint-disable-next-line react/prop-types
+const AddNewBookForm = ({success, error}) => {
+  const [addBookForm, setAddBookForm] = useState({
+    id: undefined,
+    isbn: '',
+    rating: 0,
+    art: '' || undefined,
+    preis: '' || undefined,
+    rabatt: '' || undefined,
+    lieferbar: true || false,
+    datum: new Date().toISOString().split('T')[0] | undefined,
+    homepage: '' || undefined,
+    titel: {
+      titel: '',
+      untertitel: '',
+    },
+    schlagwoerter: [],
   });
-  
-  const TableRow = styled('tr')({
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    height: 'auto',
-  });
-  
-  const TableCell = styled('td')({
-    border: '5px solid #ddd',
-    padding: '8px',
-    flex: 3,
-    width: 'auto',
-    height: 'auto',
-    minWidth: '50%',
-  });
+  const { cToken } = useContext(AuthContext);
+  const url = '/api/rest';
 
+  console.log(addBookForm);
+
+  const handleAddNewBook = async () => {
+    if (!cToken) {
+      throw new Error('No token available');
+    }
+
+    const headers = {
+      Authorization: `Bearer ${cToken}`,
+      'Content-Type': 'application/json',
+    };
+
+    const bookDTO = {
+      isbn: addBookForm.isbn,
+      rating: parseInt(addBookForm.rating),
+      art: addBookForm.art,
+      preis: parseFloat(addBookForm.preis),
+      rabatt: parseFloat(addBookForm.rabatt),
+      lieferbar: addBookForm.lieferbar === true,
+      datum: addBookForm.datum,
+      homepage: addBookForm.homepage,
+      schlagwoerter: (addBookForm.javascript ? ['JAVASCRIPT'] : []).concat(
+        addBookForm.typescript ? ['TYPESCRIPT'] : []
+      ),
+      titel:
+        typeof addBookForm.titel === 'object'
+          ? addBookForm.titel
+          : { titel: addBookForm.titel },
+    };
+    delete addBookForm.id;
+
+    try {
+      const response = await axios.post(url, bookDTO, {
+        headers: headers,
+      });
+
+      if (response.status === 201) {
+        console.success(
+          'Buch wurde erfolgreich hinzugefügt. ID:',
+          response.data.id
+        );
+
+        setAddBookForm({ ...addBookForm, id: response.data.id });
+      } else {
+        console.error('Error occurred during POST request:', response);
+      }
+    } catch (error) {
+      console.error('Error occurred during POST request:', error.message);
+    }
+  };
+
+  const handleDateChange = (e) => {
+    const { name, value } = e.target;
+
+    const transformedValue =
+      name === 'datum' ? new Date(value).toISOString().split('T')[0] : value;
+
+    setAddBookForm(() => ({
+      ...addBookForm,
+      [name]: transformedValue,
+    }));
+  };
+
+  const handleRatingChange = (newValue) => {
+    setAddBookForm(() => ({
+      ...addBookForm,
+      rating: newValue,
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    setAddBookForm(() => ({
+      ...addBookForm,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
 
   return (
-    (
-      <div>
-        <Grid
-          container
-          spacing={2}
-          style={{ flexDirection: 'column', alignItems: 'center' }}
-        >
-          <Table>
-            <TableRow>
-              <TableCell>ISBN:</TableCell>
-              <TableCell>
-                <TextField
-                  type="text"
-                  name="isbn"
-                  value={addNewBook.isbn}
-                  onChange={handleInputChange}
-                  fullWidth
+    <div>
+      <Grid
+        container
+        spacing={2}
+        style={{ flexDirection: 'column', alignItems: 'center' }}
+      >
+        <Table>
+          <TableRow>
+            <TableCell>ISBN:</TableCell>
+            <TableCell>
+              <TextField
+                type="text"
+                name="isbn"
+                value={addBookForm.isbn}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Titel:</TableCell>
+            <TableCell>
+              <TextField
+                type="text"
+                name="titel"
+                value={addBookForm.titel.titel}
+                onChange={handleInputChange}
+                placeholder="Titel"
+                fullWidth
+              />
+              {console.log('addBook:', addBookForm)}
+              <TextField
+                type="text"
+                name="untertitel"
+                value={addBookForm.titel.untertitel || undefined}
+                onChange={handleInputChange}
+                placeholder="Untertitel"
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Rating:</TableCell>
+            <TableCell>
+              <Rating
+                name="rating"
+                value={addBookForm.rating}
+                onChange={(event, newValue) => handleRatingChange(newValue)}
+                icon={<BookIcon />}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Art:</TableCell>
+            <TableCell>
+              <Select
+                name="art"
+                value={addBookForm.art || ''}
+                onChange={(e) =>
+                  setAddBookForm({ ...addBookForm, art: e.target.value })
+                }
+                fullWidth
+              >
+                <MenuItem value="KINDLE">Kindle</MenuItem>
+                <MenuItem value="DRUCKAUSGABE">Druckausgabe</MenuItem>
+              </Select>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Datum:</TableCell>
+            <TableCell>
+              <TextField
+                type="date"
+                name="datum"
+                value={addBookForm.datum}
+                onChange={handleDateChange}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Preis:</TableCell>
+            <TableCell>
+              <TextField
+                type="text"
+                name="preis"
+                value={addBookForm.preis}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Rabatt:</TableCell>
+            <TableCell>
+              <TextField
+                type="text"
+                name="rabatt"
+                value={addBookForm.rabatt}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Lieferbar:</TableCell>
+            <TableCell>
+              <RadioGroup
+                name="lieferbar"
+                value={addBookForm.lieferbar}
+                onChange={handleInputChange}
+                fullWidth
+              >
+                <FormControlLabel value={true} control={<Radio />} label="Ja" />
+                <FormControlLabel
+                  value={false}
+                  control={<Radio />}
+                  label="Nein"
                 />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Titel:</TableCell>
-              <TableCell>
-                <TextField
-                  type="text"
-                  name="titel"
-                  value={addNewBook.titel.titel}
-                  onChange={handleInputChange}
-                  placeholder="Titel"
-                  fullWidth
-                />
-                {console.log('addNewBook:', addNewBook)}
-                <TextField
-                  type="text"
-                  name="untertitel"
-                  value={addNewBook.titel.untertitel || undefined}
-                  onChange={handleInputChange}
-                  placeholder="Untertitel"
-                  fullWidth
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Rating:</TableCell>
-              <TableCell>
-                <Rating
-                  name="rating"
-                  value={addNewBook.rating}
-                  onChange={handleInputChange}
-                  icon={<BookIcon />}
-                  fullWidth
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Art:</TableCell>
-              <TableCell>
-                <Select
-                  name="art"
-                  value={addNewBook.art}
-                  onChange={handleInputChange}
-                  fullWidth
-                >
-                  <MenuItem value="KINDLE">Kindle</MenuItem>
-                  <MenuItem value="DRUCKAUSGABE">Druckausgabe</MenuItem>
-                </Select>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Datum:</TableCell>
-              <TableCell>
-                <TextField
-                  type="date"
-                  name="datum"
-                  value={addNewBook.datum}
-                  onChange={handleInputChange}
-                  fullWidth
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Preis:</TableCell>
-              <TableCell>
-                <TextField
-                  type="text"
-                  name="preis"
-                  value={addNewBook.preis}
-                  onChange={handleInputChange}
-                  fullWidth
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Rabatt:</TableCell>
-              <TableCell>
-                <TextField
-                  type="text"
-                  name="rabatt"
-                  value={addNewBook.rabatt}
-                  onChange={handleInputChange}
-                  fullWidth
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Lieferbar:</TableCell>
-              <TableCell>
-                <RadioGroup
-                  name="lieferbar"
-                  value={addNewBook.lieferbar}
-                  onChange={handleInputChange}
-                  fullWidth
-                >
-                  <FormControlLabel value="ja" control={<Radio />} label="Ja" />
-                  <FormControlLabel
-                    value="nein"
-                    control={<Radio />}
-                    label="Nein"
-                  />
-                </RadioGroup>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Homepage:</TableCell>
-              <TableCell>
-                <TextField
-                  type="text"
-                  name="homepage"
-                  value={addNewBook.homepage}
-                  onChange={handleInputChange}
-                  fullWidth
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Schlagwörter: </TableCell>
-              <TableCell>
+              </RadioGroup>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Homepage:</TableCell>
+            <TableCell>
+              <TextField
+                type="text"
+                name="homepage"
+                value={addBookForm.homepage}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Schlagwörter: </TableCell>
+            <TableCell>
+              <Grid item xs={1} sm={1}>
                 <FormControlLabel
                   control={
                     <Checkbox
-                      name="javascript"
-                      checked={addNewBook.schlagwoerter.javascript}
+                      checked={addBookForm.isJavascript}
+                      value={'javascript'}
                       onChange={handleInputChange}
-                    />
-                  }
-                  label="JavaScript"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      name="typescript"
-                      checked={addNewBook.schlagwoerter.typescript}
-                      onChange={handleInputChange}
+                      color="primary"
                     />
                   }
                   label="TypeScript"
                 />
-              </TableCell>
-            </TableRow>
-          </Table>
-          <Button
-            variant="contained"
-            style={{ alignItems: 'center', width: 'auto' }}
-            onClick={handleAddNewBook}
-          >
-            Buch anlegen
-          </Button>
-          {success && <p style={{ color: 'green' }}>{success}</p>}
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-        </Grid>
-      </div>
-    )
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={addBookForm.isTypescript}
+                      value={'typescript'}
+                      onChange={handleInputChange}
+                      color="primary"
+                    />
+                  }
+                  label="JavaScript"
+                />
+              </Grid>
+            </TableCell>
+          </TableRow>
+        </Table>
+        <Button
+          variant="contained"
+          style={{ alignItems: 'center', width: 'auto' }}
+          onClick={() => handleAddNewBook()}
+        >
+          Buch anlegen
+        </Button>
+        {success && <p style={{ color: 'green' }}>{success}</p>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </Grid>
+    </div>
   );
 };
+const Table = styled('table')({
+  width: '75%',
+  display: 'relative',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  margin: '0',
+  flexWrap: 'wrap',
+});
+
+const TableRow = styled('tr')({
+  display: 'flex',
+  flexDirection: 'row',
+  width: '100%',
+  height: 'auto',
+});
+
+const TableCell = styled('td')({
+  border: '5px solid #ddd',
+  padding: '8px',
+  flex: 3,
+  width: 'auto',
+  height: 'auto',
+  minWidth: '50%',
+});
 
 AddNewBookForm.propTypes = {
-  addNewBook: PropTypes.shape({
-    isbn: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-    art: PropTypes.string.isRequired,
-    preis: PropTypes.string.isRequired,
-    rabatt: PropTypes.string.isRequired,
-    lieferbar: PropTypes.oneOf(['ja', 'nein']).isRequired,
-    datum: PropTypes.string.isRequired,
-    homepage: PropTypes.string.isRequired,
-    schlagwoerter: PropTypes.arrayOf(PropTypes.string).isRequired,
-    titel: PropTypes.shape({
-      titel: PropTypes.string.isRequired,
-      untertitel: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
+  addNewBook: PropTypes.shape({}).isRequired,
   handleInputChange: PropTypes.func.isRequired,
   handleAddNewBook: PropTypes.func.isRequired,
+  handleDateChange: PropTypes.func.isRequired,
+  handleRatingChange: PropTypes.func.isRequired,
   success: PropTypes.bool,
   error: PropTypes.string,
 };
--- src/components/main/AddNewBook.jsx
+++ src/components/main/AddNewBook.jsx
@@ -1,84 +1,12 @@
-import { useContext, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 import AddNewBookForm from '../form/AddNewBookForm';
-import { AuthContext } from '../provider/AuthProvider';
 
 const AddNewBook = () => {
-  const [addNewBook, setAddNewBook] = useState({
-    isbn: '',
-    rating: 0,
-    art: '',
-    preis: '',
-    rabatt: '',
-    lieferbar: 'ja',
-    datum: new Date().toISOString().split('T')[0],
-    homepage: '',
-    titel: {
-      titel: '',
-      untertitel: '' | undefined,
-    },
-    schlagwoerter: {
-      javascript: '',
-      typescript: '',
-    },
-  });
-
-  const { cToken } = useContext(AuthContext);
-  const [success, setSuccess] = useState(null);
-  const [error, setError] = useState(null);
-
-  const handleAddNewBook = async () => {
-    try {
-      const headers = {
-        Authorization: `Bearer ${cToken}`,
-        'Content-Type': 'application/json',
-      };
-
-      const response = await axios.post('/api/rest', addNewBook, {
-        headers,
-      });
-
-      if (!addNewBook.isbn || !addNewBook.titel.titel) {
-        setSuccess(false);
-        setError('Bitte fülle alle erforderlichen Felder aus.');
-        return;
-      }
-
-      console.log('Server Response:', response);
-      if (response.status !== 201) {
-        setSuccess(false);
-        setError(`Fehler beim Hinzufügen des Buchs: ${response.statusText}`);
-      } else if (response.data) {
-        setSuccess(true);
-        setError(false);
-      }
-    } catch (error) {
-      setSuccess(false);
-      setError(`Fehler beim Hinzufügen des Buchs: ${error.message}`);
-    }
-
-    console.log('Neues Buch:', addNewBook);
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setAddNewBook ({
-      ...addNewBook,
-      [name]: type === 'checkbox' ? checked : value,
-    });
-  };
-
-  console.log('Neues Buch erstellt: ' , AddNewBookForm)
+  const [addNewBook] = useState();
 
   return (
     <div>
-      <AddNewBookForm
-        addNewBook={addNewBook}
-        handleInputChange={handleInputChange}
-        handleAddNewBook={handleAddNewBook}
-        success={success}
-        error={error}
-      />
+      <AddNewBookForm addNewBook={addNewBook} />
     </div>
   );
 };
